e72fb1df8aaaa31adef517945148aef7d63df749
Joshua McKinnon <flxwedd@gmail.com>
2025-07-25 03:11:06 -0700

    Initial commit

---
+++ fabric/src/main/java/dev/lrockreal/lootlog/fabric/LootLogFabric.java
@@ -0,0 +1,32 @@
+/*
+ * Copyright 2025 Joshua McKinnon
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package dev.lrockreal.lootlog.fabric;
+
+import dev.lrockreal.lootlog.LootLog;
+import net.fabricmc.api.ModInitializer;
+
+public final class LootLogFabric implements ModInitializer {
+    @Override
+    public void onInitialize() {
+        // This code runs as soon as Minecraft is in a mod-load-ready state.
+        // However, some things (like resources) may still be uninitialized.
+        // Proceed with mild caution.
+
+        // Run our common setup.
+        LootLog.init();
+    }
+}
